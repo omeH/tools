7239841df5a4f8181b6f08861022fa0d73620789
omeH <hanulllu@gmail.com>
2014-10-01 11:25:36 +0300

    Added option to select a version pip

--- python2/upgradeallpackges.py
+++ python2/upgradeallpackges.py
@@ -1,11 +1,22 @@
+import argparse
 import pip
 from subprocess import call
 
 
-def upgrade():
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-v', '--version', type=str, default='2',
+                        help='Version pip')
+    return parser.parse_args()
+
+
+def upgrade(version):
+    name_pip = 'pip{0}'.format(version)
     for dist in pip.get_installed_distributions():
-        call('pip install --upgrade ' + dist.project_name, shell=True)
+        call(name_pip + ' install --upgrade ' + dist.project_name, shell=True)
 
 
 if __name__ == '__main__':
-    upgrade()
+    options = parse_args()
+    upgrade(options.version)
